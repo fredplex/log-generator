3f74d707f9a1a0ca1ed699fa0aaaf7a707d1fb8b
Andrew Low <Andrew_Low@ca.ibm.com>
2024-06-14 08:21:43 -0400

    Create app.go

---
+++ app.go
@@ -0,0 +1,28 @@
+// Based on https://github.com/IBM/CodeEngine/blob/main/app-n-job/app-n-job.go 
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+  // JOB_INDEX env var is a hint about the mode (batch/application) that code engine is running in
+	jobIndex := os.Getenv("JOB_INDEX")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("log: app-n-job got an incoming request\n")
+		fmt.Fprintf(w, "Hello from app-n-job! I'm a web app!\n")
+	})
+
+	if jobIndex == "" {
+		log.Printf("Listening on port 8080\n")
+		http.ListenAndServe(":8080", nil)
+	} else {
+		log.Printf("Hello from app-n-job! I'm a batch job! Index: %s\n",
+			jobIndex)
+	}
+}
